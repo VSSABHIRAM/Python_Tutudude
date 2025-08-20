064fc62344475bc0c3c50f843c83e499f8c27021
VSSABHIRAM <163302517+VSSABHIRAM@users.noreply.github.com>
2025-08-20 18:57:50 +0530

    Add files via upload

---
+++ Python/Assignment-1/Assignment-1.py
@@ -0,0 +1,15 @@
+# 1)
+x = int(input('Enter the first number: '))
+y = int(input('Enter the second number: '))
+
+print('Addition:', x + y)
+print('Subtraction:', x - y)
+print('Multiplication:', x * y)
+print('Division:', x / y)
+
+# 2)
+
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+
+print(f'Hello, {first_name} {last_name}! Welcome to the Python program.')
